154d162c2cac118e2a2fb674adcbc856e79588a6
Pankaj Panjiyar <pankaj.panjiyar@gmail.com>
2012-02-26 17:35:54 +0545

    Fresh Copy

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="keywords" content="tender, tenders, tenders of nepal, tender of nepal, tender from nepal, nepal tender, tenders from nepal, tendernepal, nepaltender, tender in nepal, tenders in nepal, nepali tender, contracts, latest tenders, corrigendum, agendas, civil, construction, transport, health, road, bridge, information technology, computer, goverment, private, corporate, others, fmcg, pipeline, consultancy,printing, tender by location,tender by value, closed tenders, agroproducts, sitemap, shangrila, shangrilainc, shangrila software, shangrila technology, software company in nepal, list of software company in nepal, nepal, nepal telecom tender, nepal telecom" />
+<meta name="publisher" content="tendernepal.com" />
+<meta name="copyright" content="Shangrila Software" />
+<meta name="author" content="Shangrila Software" />
+<meta name="distribution" content="global" />
+<meta name="description" content="Shangrila Software" />
+<title>Tendernepal.... The ultimate portal for all tenders published in Nepal</title>
+<link rel="shortcut icon" href="images/favicon.png" type="images/x-icon" />
+<link href="stylesheet/global.css" type="text/css" rel="stylesheet" media="all"/>
+</head>
+<body>
+<div id="main">
+	<div id="header">
+		<?php 
+			include 'header.php';
+		?>
+	</div>
+	<div>
+		<?php
+			include 'home.php';
+		?>
+	</div>
+	<div style="clear:both;">
+		<?php 
+			include 'footer.php';
+		?>
+	</div>	
+</div>
+<script language="javascript" type="text/javascript">
+          var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+          document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script language="javascript" type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-12287309-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
